27cf4d27a915a6408ba0b49c41389e6f26ab5c4b
Frode Petterson <frode.andre.petterson@gmail.com>
2016-05-31 13:28:34 +0200

    Revert "Made sure padding and border are included in width and height for draggables [HFJ-1870]"
    This reverts commit 0b458e5744654b7352d8eb3958e016344c1fa0ac.
    
    Conflicts:
    	library.json

--- upgrades.js
+++ upgrades.js
@@ -30,57 +30,22 @@ H5PUpgrades['H5P.DragQuestion'] = (function ($) {
        * @params {Object} parameters
        * @params {function} finished
        */
-      4: {
-        contentUpgrade: function (parameters, finished) {
-          if (parameters.question !== undefined && parameters.question.task !== undefined && parameters.question.task.elements !== undefined) {
-            var elements = parameters.question.task.elements;
+      4: function (parameters, finished) {
+        if (parameters.question !== undefined && parameters.question.task !== undefined && parameters.question.task.elements !== undefined) {
+          var elements = parameters.question.task.elements;
 
-            // Go through elements
-            for (var i = 0; i < elements.length; i++) {
-              var element = elements[i];
+          // Go through elements
+          for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
 
-              // Check if element type is text
-              if (element && element.type && element.type.library &&
-                  element.type.library.split(' ')[0] === 'H5P.Text') {
-                element.type.library = 'H5P.AdvancedText 1.0';
-              }
+            // Check if element type is text
+            if (element && element.type && element.type.library &&
+                element.type.library.split(' ')[0] === 'H5P.Text') {
+              element.type.library = 'H5P.AdvancedText 1.0';
             }
           }
-          finished(null, parameters);
         }
-      },
-      /**
-       * Asynchronous content upgrade hook.
-       * Upgrades content parameters to support DQ 1.6.
-       *
-       * Makes width and height be the same as in 1.5
-       *
-       * @params {Object} parameters
-       * @params {function} finished
-       */
-      6: {
-        contentUpgrade: function (parameters, finished) {
-          if (parameters.question !== undefined && parameters.question.task !== undefined && parameters.question.task.elements !== undefined) {
-            var elements = parameters.question.task.elements;
-
-            // Go through elements
-            for (var i = 0; i < elements.length; i++) {
-              var element = elements[i];
-
-              // The magic number 0.8 is the vertical/horizontal padding + border
-              // in ems (0.1 + 0.3 + 0.3 + 0.1)
-              if (element) {
-                if (element.width) {
-                  element.width += 0.8;
-                }
-                if (element.height) {
-                  element.height += 0.8;
-                }
-              }
-            }
-          }
-          finished(null, parameters);
-        }
+        finished(null, parameters);
       }
     }
   };
