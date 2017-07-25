9507c65c0733f0432ce1aa439714f51c642e7690
Frode Petterson <frode.andre.petterson@gmail.com>
2017-07-25 13:42:54 +0200

    Do not display scorepoints if singlepoint is enabled
    HFP-1262

--- src/drag-question.js
+++ src/drag-question.js
@@ -747,7 +747,7 @@ C.prototype.showAllSolutions = function (skipVisuals) {
   this.rawPoints += emptyDropzones;
 
   var scorePoints;
-  if (!skipVisuals && this.options.behaviour.showScorePoints) {
+  if (!skipVisuals && this.options.behaviour.showScorePoints && !this.options.behaviour.singlePoint) {
     scorePoints = new H5P.Question.ScorePoints();
   }
 
