ffca6e6cb1102a89d513f8511a7f402d3dc1bf0c
Thomas Horn Sivertsen <thomass@cerpus.com>
2018-03-27 16:20:35 +0200

    Added/modified calculation of max score

--- presave.js
+++ presave.js
@@ -1,61 +1,72 @@
+var H5PEditor = H5PEditor || {};
 var H5PPresave = H5PPresave || {};
 
 H5PPresave['H5P.DragQuestion'] = function (content, finished) {
-    if (typeof content === "undefined" || !content.hasOwnProperty('question') || !content.question.hasOwnProperty('task')) {
-        throw {
-            name: 'Invalid Drag and Drop Error',
-            message: "Could not find expected semantics in content."
-        };
-    }
+  var presave = H5PEditor.Presave;
+  var score = 0;
+  var correctDropZones = [];
+
+  if (isContentInValid()) {
+    throw new presave.exceptions.InvalidContentSemanticsException('Invalid Drag and Drop Error')
+  }
+
+  if (hasDropZones()) {
+    correctDropZones = content.question.task.dropZones
+      .map(function (dropzone) {
+        return dropzone.correctElements;
+      })
+      .filter(function (correctElements) {
+        return correctElements.length;
+      })
+      .reduce(function (previous, current, dropZone) {
+        current.forEach(function (element) {
+          if (!Array.isArray(previous[element])) {
+            previous[element] = [];
+          }
+          previous[element].push(dropZone);
+        });
+        return previous;
+      }, []);
+  }
 
 
-    var score = 0;
-    var correctDropZones = content.question.task.hasOwnProperty('dropZones') ?
-        content.question.task.dropZones
-            .map(function (dropzone) {
-                return dropzone.correctElements;
-            })
-            .filter(function (correctElements) {
-                return correctElements.length;
-            })
-            .reduce(function (previous, current, dropZone) {
-                current.forEach(function (element) {
-                    if( !Array.isArray(previous[element])){
-                        previous[element] = [];
-                    }
-                    previous[element].push(dropZone);
-                });
-                return previous;
-            }, []) :
-        [];
+  if (correctDropZones.length === 0 || isSinglePoint()) {
+    score = 1;
+  } else if (hasElements()) {
+    score = content.question.task.elements
+      .filter(function (element, index) {
+        return Array.isArray(correctDropZones[index]) && correctDropZones.length > 0;
+      })
+      .map(function (element, index) {
+        if (element.multiple === true) {
+          return correctDropZones.length;
+        }
+        return 1;
+      })
+      .reduce(function (previous, current) {
+        return previous + current;
+      }, 0);
+  }
 
-    if (correctDropZones.length === 0) {
-        score = 1;
-    } else {
-        score = content.question.task.hasOwnProperty('elements') ?
-            content.question.task.elements
-                .filter(function (element, index) {
-                    return Array.isArray(correctDropZones[index]) && correctDropZones.length > 0;
-                })
-                .map(function (element, index) {
-                    if( element.multiple === true ){
-                        return correctDropZones.length;
-                    }
-                    return 1;
-                })
-                .reduce(function (previous, current) {
-                    return previous + current;
-                }, 0)
-            : null;
+  presave.validateScore(score);
 
-    }
-
-    if (isNaN(score) || score < 0) {
-        throw {
-            name: 'InvalidMaxScore Error',
-            message: "Could not calculate the max score for this content. The max score is assumed to be 0. Contact your administrator if this isn’t correct."
-        };
-    }
-
+  if (finished) {
     finished({maxScore: score});
+  }
+
+  function isContentInValid() {
+    return !presave.checkNestedRequirements(content, 'content.question.task');
+  }
+
+  function hasDropZones() {
+    return presave.checkNestedRequirements(content, 'content.question.task.dropZones') && Array.isArray(content.question.task.dropZones);
+  }
+
+  function hasElements() {
+    return presave.checkNestedRequirements(content, 'content.question.task.elements') && Array.isArray(content.question.task.elements);
+  }
+
+  function isSinglePoint() {
+    return presave.checkNestedRequirements(content, 'content.behaviour.singlePoint') && content.behaviour.singlePoint === true;
+  }
 };
